72266812d67b5393efce559a51c787a1ce7d26e1
ruedasan <125039095+ruedasan@users.noreply.github.com>
2023-06-05 12:03:00 -0500

    Add files via upload

--- taller3/robot_manipulador_lego.py
+++ taller3/robot_manipulador_lego.py
@@ -7,9 +7,9 @@ from geometry_msgs.msg import Vector3
 from scipy.optimize import fsolve
 
 
-class RobotManipulatorLego(Node):
+class RobotManipulatorPlanner(Node):
     def __init__(self):
-        super().__init__('robot_manipulator_lego')
+        super().__init__('robot_manipulator_planner')
         self.subscription = self.create_subscription(
             Vector3,
             '/robot_manipulator_zone',
@@ -22,22 +22,14 @@ class RobotManipulatorLego(Node):
 
         self.arduino = serial.Serial(port=arduino_port, baudrate=250000, timeout=.1)
     
-    def lego(self):
+    def goal_callback(self, msg):
+        target_x = msg.x 
+        target_y = msg.y 
+        z = msg.z
 
-        L1 = 12.2
-        L2 = 15
 
         
-        theta1 = 90 
-        theta2 = 180
-        theta3= 170
-
-        x1 = L1 * math.cos(math.radians(theta1)) + L2 * math.cos(math.radians(theta1 + theta2))
-        y1 = L1 * math.sin(math.radians(theta1)) + L2 * math.sin(math.radians(theta1 + theta2))
-        print(x1,y1)
-
-        mensaje = f'{theta1},{theta2},{theta3}'
-        print(mensaje)
+        mensaje = 1
 
         self.write(mensaje)
 
@@ -49,9 +41,9 @@ class RobotManipulatorLego(Node):
 
 def main(args=None):
     rclpy.init(args=args)
-    robot_manipulator_lego = RobotManipulatorLego()
-    rclpy.spin(robot_manipulator_lego)
-    robot_manipulator_lego.destroy_node()
+    robot_manipulator_planner = RobotManipulatorPlanner()
+    rclpy.spin(robot_manipulator_planner)
+    robot_manipulator_planner.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
